0ea7893b3ecf3d3d21e616c21c3bd5a68bd34967
Dominik Drees <dominik.drees@wwu.de>
2022-11-18 17:04:20 +0100

    Add rudimentary .vvd reader

--- src/operators/reader/vvd.rs
+++ src/operators/reader/vvd.rs
@@ -1,8 +1,7 @@
-use hard_xml::XmlRead;
-use std::{
-    borrow::Cow,
-    path::{Path, PathBuf},
-};
+use sxd_document::*;
+use sxd_xpath::evaluate_xpath;
+
+use std::path::{Path, PathBuf};
 
 use crate::{
     data::{SVec3, VolumeMetaData, VoxelPosition},
@@ -13,56 +12,45 @@ use crate::{
 
 use super::RawVolumeSource;
 
-#[derive(XmlRead, PartialEq, Debug)]
-#[xml(tag = "VoreenData")]
-struct VoreenData<'a> {
-    #[xml(attr = "version")]
-    version: Cow<'a, str>,
-
-    #[xml(child = "Volumes")]
-    volumes: Vec<Volume<'a>>,
-}
-
-#[derive(XmlRead, PartialEq, Debug)]
-#[xml(tag = "Volume")]
-struct Volume<'a> {
-    #[xml(child = "filename")]
-    raw_data: RawData<'a>,
-}
-
-#[derive(XmlRead, PartialEq, Debug)]
-#[xml(tag = "RawData")]
-struct RawData<'a> {
-    #[xml(attr = "filename")]
-    file_name: Cow<'a, str>,
-
-    #[xml(attr = "format")]
-    format: Cow<'a, str>,
-
-    #[xml(attr = "x")]
-    x: u32,
-
-    #[xml(attr = "y")]
-    y: u32,
-
-    #[xml(attr = "z")]
-    z: u32,
-}
-
 pub struct VvdVolumeSource {
     raw: RawVolumeSource,
 }
 
+fn find_valid_path(base: Option<&Path>, val: &sxd_xpath::Value) -> Option<PathBuf> {
+    let sxd_xpath::Value::Nodeset(set) = val else {
+        return None;
+    };
+
+    for v in set {
+        let v = v.string_value();
+        let path = PathBuf::from(v);
+        if path.exists() {
+            return Some(path);
+        }
+        if let Some(base) = base {
+            let from_base = base.join(path);
+            if from_base.exists() {
+                return Some(from_base);
+            }
+        }
+    }
+    None
+}
+
 impl VvdVolumeSource {
     pub fn open(path: &Path, brick_size: VoxelPosition) -> Result<Self, Error> {
-        let content = std::fs::read_to_string(path).unwrap();
-        let metadata = VoreenData::from_str(&content)?;
+        let content = std::fs::read_to_string(path)?;
+        let package = parser::parse(&content)?;
+        let document = package.as_document();
 
-        let vol = &metadata.volumes[0];
-        let raw_data = &vol.raw_data;
+        let x = evaluate_xpath(&document, "/VoreenData/Volumes/Volume/RawData/@x")?.number() as _;
+        let y = evaluate_xpath(&document, "/VoreenData/Volumes/Volume/RawData/@y")?.number() as _;
+        let z = evaluate_xpath(&document, "/VoreenData/Volumes/Volume/RawData/@z")?.number() as _;
+        let format =
+            evaluate_xpath(&document, "/VoreenData/Volumes/Volume/RawData/@format")?.string();
 
-        let size = VoxelPosition(SVec3::new(raw_data.x, raw_data.y, raw_data.z));
-        let format = &raw_data.format;
+        let size = VoxelPosition(SVec3::new(x, y, z));
+
         if format != "float" {
             return Err(format!(
                 "Unsupported format '{}'. Only float volumes are supported currently",
@@ -70,12 +58,25 @@ impl VvdVolumeSource {
             )
             .into());
         }
+
+        let simple_path =
+            evaluate_xpath(&document, "/VoreenData/Volumes/Volume/RawData/@filename")?;
+        let alternative_paths = evaluate_xpath(
+            &document,
+            "/VoreenData/Volumes/Volume/RawData/Paths/paths/item/@value",
+        )?;
+
+        let base = path.parent();
+        let Some(raw_path) = find_valid_path(base, &simple_path).or_else(|| find_valid_path(base, &alternative_paths)) else {
+            return Err("No valid .raw file path in file".into());
+        };
+
         let vmd = VolumeMetaData {
             dimensions: size,
             brick_size,
         };
 
-        let raw = RawVolumeSource::open(PathBuf::from(raw_data.file_name.as_ref()), vmd)?;
+        let raw = RawVolumeSource::open(raw_path, vmd)?;
         Ok(VvdVolumeSource { raw })
     }
 }
