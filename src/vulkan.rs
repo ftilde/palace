97f3e071728019ff72c56583e51a84903166b4bc
Hennes Rave <hennes.rave@uni-muenster.de>
2023-01-29 16:46:35 +0100

    Implemented vulkan device context

--- src/vulkan.rs
+++ src/vulkan.rs
@@ -1,8 +1,13 @@
 use crate::Error;
-use std::borrow::Cow;
+use std::any::Any;
+use std::cell::Ref;
+use std::cell::RefCell;
+use std::borrow::{Cow, Borrow};
+use std::collections::HashMap;
 use std::ffi::{CStr, c_char};
-use ash::vk;
+use ash::vk::{self, DeviceGroupDeviceCreateInfo};
 use ash::extensions::ext::DebugUtils;
+use ndarray::IndexLonger;
 
 unsafe extern "system" fn vulkan_debug_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
@@ -39,11 +44,7 @@ pub struct VulkanManager {
     debug_utils_loader: DebugUtils,
     debug_callback: vk::DebugUtilsMessengerEXT,
 
-    physical_device: vk::PhysicalDevice,
-    queue_family_index: u32,
-    queue_count: u32,
-
-    device : ash::Device
+    device_contexts: Vec<DeviceContext>,
 }
 
 impl VulkanManager {
@@ -90,28 +91,18 @@ impl VulkanManager {
                 .create_debug_utils_messenger(&create_info, None)
                 .unwrap();
 
-            // Find suitable physical device
+            // Create device contexts
             let physical_devices = instance.enumerate_physical_devices().expect("Failed to enumerate physical devices.");
-            let (physical_device, queue_family_index, queue_count) = physical_devices.iter().find_map(|physical_device| {
+            let device_contexts: Vec<DeviceContext> = physical_devices.iter().filter_map(|physical_device| {
                 instance.get_physical_device_queue_family_properties(*physical_device).iter().enumerate().find_map(|(index, info)| {
                     if info.queue_flags.contains(vk::QueueFlags::GRAPHICS | vk::QueueFlags::COMPUTE) {
-                        Some((*physical_device, index as u32, info.queue_count))
+                        DeviceContext::new(&instance, *physical_device, index as u32, info.queue_count).ok()
                     } else {
                         None
                     }
                 })
-            }).expect("Failed to find suitable physical device.");
-
-            // Create device
-            let queue_priorities = vec![0 as f32; queue_count as usize];
-            let queue_create_info = vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(queue_family_index)
-                .queue_priorities(&queue_priorities);
-            let enabled_features = vk::PhysicalDeviceFeatures::builder();
-            let create_info = vk::DeviceCreateInfo::builder()
-                .queue_create_infos(std::slice::from_ref(&queue_create_info))
-                .enabled_features(&enabled_features);
-            let device = instance.create_device(physical_device, &create_info, None).expect("Device creation failed.");
+            }).collect();
+            assert!(!device_contexts.is_empty(), "Unable to find suitable physical devices.");
 
             // Return VulkanManager
             println!("Finished initializing Vulkan!");
@@ -123,11 +114,7 @@ impl VulkanManager {
                 debug_utils_loader,
                 debug_callback,
 
-                physical_device,
-                queue_family_index,
-                queue_count,
-
-                device
+                device_contexts
             })
         }
     }
@@ -136,9 +123,159 @@ impl VulkanManager {
 impl Drop for VulkanManager {
     fn drop(&mut self) {
         unsafe {
-            self.device.destroy_device(None);
+            for device_context in &self.device_contexts {
+                device_context.deinitialize();
+            }
+
             self.debug_utils_loader.destroy_debug_utils_messenger(self.debug_callback, None);
             self.instance.destroy_instance(None);
         }
     }
+}
+
+pub struct DeviceContext {
+    physical_device: vk::PhysicalDevice,
+    physical_device_memory_properties : vk::PhysicalDeviceMemoryProperties,
+    queue_family_index: u32,
+    queue_count: u32,
+    
+    pub device: ash::Device,
+    queues: Vec<vk::Queue>,
+
+    command_pool: vk::CommandPool,
+    command_buffers: RefCell<Vec<(vk::CommandBuffer, vk::Fence)>>,
+
+    vulkan_states: RefCell<HashMap<String, Box<dyn Any>>>
+}
+
+impl DeviceContext {
+    pub fn new(instance: &ash::Instance, physical_device: vk::PhysicalDevice, queue_family_index: u32, queue_count: u32) -> Result<Self, Error> {
+        unsafe {
+            let physical_device_memory_properties = instance.get_physical_device_memory_properties(physical_device);
+
+            // Create logical device
+            let queue_priorities = vec![0 as f32; queue_count as usize];
+            let queue_create_info = vk::DeviceQueueCreateInfo::builder()
+                .queue_family_index(queue_family_index)
+                .queue_priorities(&queue_priorities);
+            let enabled_features = vk::PhysicalDeviceFeatures::builder();
+            let create_info = vk::DeviceCreateInfo::builder()
+                .queue_create_infos(std::slice::from_ref(&queue_create_info))
+                .enabled_features(&enabled_features);
+            let device = instance.create_device(physical_device, &create_info, None).expect("Device creation failed.");
+
+            // Get device queues
+            let queues: Vec<vk::Queue> = (0..queue_count).map(|index|{
+                device.get_device_queue(queue_family_index, index)
+            }).collect();
+
+            // Create command pool
+            let create_info = vk::CommandPoolCreateInfo::builder()
+                .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)
+                .queue_family_index(queue_family_index);
+            let command_pool = device.create_command_pool(&create_info, None).expect("Command pool creation failed.");
+            let command_buffers = RefCell::new(Vec::new());
+
+            let vulkan_states = RefCell::new(HashMap::new());
+
+            Ok(DeviceContext {
+                physical_device,
+                physical_device_memory_properties,
+                queue_family_index,
+                queue_count,
+
+                device,
+                queues,
+
+                command_pool,
+                command_buffers,
+
+                vulkan_states
+            })
+        }
+    }
+
+    pub fn request_state<T: VulkanState + Default + 'static>(&self, identifier: &String) -> Ref<'_, Box<T>> {
+        if !self.vulkan_states.borrow().contains_key(identifier) {
+            let state = Box::new(T::default());
+            state.initialize(self);
+            self.vulkan_states.borrow_mut().insert(identifier.clone(), state);
+        }
+        
+        Ref::map(self.vulkan_states.borrow(), |state| state.get(identifier).unwrap().downcast_ref::<Box<T>>().expect("wrong type"))
+    }
+
+    pub fn find_memory_type_index(&self,
+        memory_requirements : &vk::MemoryRequirements,
+        memory_property_flags: vk::MemoryPropertyFlags 
+    ) -> Option<u32> {
+        self.physical_device_memory_properties.memory_types[..self.physical_device_memory_properties.memory_type_count as _]
+            .iter()
+            .enumerate()
+            .find(|(index, memory_type)| {
+                (0x1 << index) & memory_requirements.memory_type_bits != 0 && memory_type.property_flags & memory_property_flags == memory_property_flags
+            }).map(|(index, _memory_type)| index as _)
+    }
+
+
+    pub fn begin_command_buffer(&self) -> vk::CommandBuffer {
+        unsafe {
+            for (command_buffer, fence) in self.command_buffers.borrow().iter() {
+                if self.device.get_fence_status(*fence).unwrap_or(false) {
+                    let begin_info = vk::CommandBufferBeginInfo::builder().flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
+                    self.device.begin_command_buffer(*command_buffer, &begin_info).expect("Failed to begin command buffer.");
+                    return *command_buffer
+                }
+            }
+
+            // Create command buffer
+            let create_info = vk::CommandBufferAllocateInfo::builder()
+                .command_pool(self.command_pool)
+                .command_buffer_count(1);
+            let command_buffer = *self.device.allocate_command_buffers(&create_info).expect("Failed to allocate command buffer.").first().unwrap();
+
+            // Create fence
+            let create_info = vk::FenceCreateInfo::default();
+            let fence = self.device.create_fence(&create_info, None).expect("Failed to create fence.");
+
+            self.command_buffers.borrow_mut().push((command_buffer, fence));
+
+            let begin_info = vk::CommandBufferBeginInfo::builder().flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
+            self.device.begin_command_buffer(command_buffer, &begin_info).expect("Failed to begin command buffer.");
+            return command_buffer
+        }
+    }
+    pub fn submit_command_buffer(&self, command_buffer: vk::CommandBuffer) -> vk::Fence {        
+        unsafe {
+            for (other, fence) in self.command_buffers.borrow().iter() {
+                if command_buffer == *other {
+                    let submits = [vk::SubmitInfo::builder().command_buffers(std::slice::from_ref(&command_buffer)).build()];
+                    self.device.queue_submit(*self.queues.first().unwrap(), &submits, *fence).expect("Failed to submit command buffers to queue.");
+
+                    self.device.wait_for_fences(std::slice::from_ref(fence), true, u64::max_value()).expect("Failed to wait for fence.");
+                    self.device.reset_fences(std::slice::from_ref(fence)).expect("Failed to reset fence.");
+                    self.device.reset_command_buffer(command_buffer, vk::CommandBufferResetFlags::empty()).expect("Failed to reset command buffer.");
+        
+                    return *fence        
+                }
+            }
+            panic!("Tried to submit unknown command buffer.");
+        }
+    }
+
+    pub fn deinitialize(&self) {
+        unsafe {
+            for (_identifier, vulkan_state) in self.vulkan_states.borrow().iter() {
+                vulkan_state.downcast_ref::<Box<dyn VulkanState>>().unwrap().deinitialize(self);
+            }
+
+            self.device.destroy_command_pool(self.command_pool, None);
+            self.device.destroy_device(None);
+        }
+    }
+}
+
+pub trait VulkanState {
+    fn initialize(&self, context: &DeviceContext);
+    fn deinitialize(&self, context: &DeviceContext);
 }
