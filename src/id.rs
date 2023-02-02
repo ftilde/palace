03293254dfdccc2f561c0ffa6a0418ab38ee02aa
Dominik Drees <dominik.drees@wwu.de>
2023-02-02 15:07:15 +0100

    Use u128 as Id representation

--- src/id.rs
+++ src/id.rs
@@ -1,19 +1,27 @@
 use sha1_smol::Sha1;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
-pub struct Id([u8; 20]);
+pub struct Id(u128);
+
+#[repr(C, align(16))]
+struct IdBytes([u8; 16]);
+
+fn digest_to_u128(v: [u8; 20]) -> u128 {
+    let bytes = IdBytes(std::array::from_fn(|i| v[i]));
+    *bytemuck::from_bytes(&bytes.0)
+}
 
 impl Id {
     pub fn from_data(data: &[u8]) -> Self {
         let sha = Sha1::from(data);
-        Self(sha.digest().bytes())
+        Self(digest_to_u128(sha.digest().bytes()))
     }
     pub fn combine(ids: &[Id]) -> Self {
         let mut sha = Sha1::new();
         for id in ids {
-            sha.update(&id.0);
+            sha.update(bytemuck::bytes_of(&id.0));
         }
-        Self(sha.digest().bytes())
+        Self(digest_to_u128(sha.digest().bytes()))
     }
 }
 
