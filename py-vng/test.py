dc6aaa06033b96d3d1458746beeb06ac0b71b9e9
Dominik Drees <dominik.drees@uni-muenster.de>
2023-10-26 17:09:08 +0200

    Clean up test.py

--- py-vng/test.py
+++ py-vng/test.py
@@ -6,8 +6,6 @@ import math
 ram_size = 8 << 30
 vram_size = 10 << 30
 
-print(vram_size)
-
 rt = vng.RunTime(ram_size, vram_size)
 
 window = vng.Window(rt)
@@ -58,42 +56,25 @@ def split(dim, fraction, render_first, render_last):
 
     return inner
 
-def render(size, events):
-    gui = gui_state.setup(events, vng.Vertical([
-        vng.Label("Look at all these buttons"),
-        vng.Horizontal([
-            vng.Button("yes?", lambda: print("yes!")),
-            vng.Button("no?", lambda: print("no!")),
-            vng.Slider(camera_state.fov(), 10, 50),
-            vng.Slider(camera_state.fov(), 20, 60),
-        ]),
-    ]))
+# Raycasting render component
+def render_raycast(size, events, camera_state):
+    def inner(size, events):
+        events.act([
+            vng.OnMouseDrag(vng.MouseButton.Left, lambda pos, delta: camera_state.trackball().mutate(lambda tb: tb.pan_around(delta))),
+            vng.OnWheelMove(lambda delta, pos: camera_state.trackball().mutate(lambda tb: tb.move_inout(delta))),
+        ]);
 
-    lower = split(vng.SplitDirection.Horizontal, 0.5, render_slice(0, slice_state0), render_slice(1, slice_state1))
-    upper = split(vng.SplitDirection.Horizontal, 0.5, render_raycast, render_slice(2, slice_state2))
+        md = vng.tensor_metadata(size, [512]*2)
+        proj = camera_state.load().projection_mat(size)
 
-    frame = split(vng.SplitDirection.Vertical, 0.5, upper, lower)(size, events)
+        eep = vng.entry_exit_points(v.metadata, md, proj)
+        frame = vng.raycast(v, eep)
+        frame = vng.rechunk(frame, [vng.chunk_size_full]*3)
 
-    frame = gui.render(frame)
+        return frame
+    return inner
 
-    return frame
-
-def render_raycast(size, events):
-
-    events.act([
-        vng.OnMouseDrag(vng.MouseButton.Left, lambda pos, delta: camera_state.trackball().mutate(lambda tb: tb.pan_around(delta))),
-        vng.OnWheelMove(lambda delta, pos: camera_state.trackball().mutate(lambda tb: tb.move_inout(delta))),
-    ]);
-
-    md = vng.tensor_metadata(size, [512]*2)
-    proj = camera_state.load().projection_mat(size)
-
-    eep = vng.entry_exit_points(v.metadata, md, proj)
-    frame = vng.raycast(v, eep)
-    frame = vng.rechunk(frame, [vng.chunk_size_full]*3)
-
-    return frame
-
+# Slice render component
 def render_slice(dim, slice_state):
     def inner(size, events):
         events.act([
@@ -111,5 +92,26 @@ def render_slice(dim, slice_state):
 
         return frame
     return inner
+
+# Top-level render component
+def render(size, events):
+    gui = gui_state.setup(events, vng.Vertical([
+        vng.Label("Look at all these buttons"),
+        vng.Horizontal([
+            vng.Button("yes?", lambda: print("yes!")),
+            vng.Button("no?", lambda: print("no!")),
+            vng.Slider(camera_state.fov(), 10, 50),
+            vng.Slider(camera_state.fov(), 20, 60),
+        ]),
+    ]))
+
+    lower = split(vng.SplitDirection.Horizontal, 0.5, render_slice(0, slice_state0), render_slice(1, slice_state1))
+    upper = split(vng.SplitDirection.Horizontal, 0.5, render_raycast(camera_state), render_slice(2, slice_state2))
+
+    frame = split(vng.SplitDirection.Vertical, 0.5, upper, lower)(size, events)
+
+    frame = gui.render(frame)
+
+    return frame
 
 window.run(render, timeout_ms=10)
