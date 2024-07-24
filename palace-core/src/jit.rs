30584a47e127ea53cf710856a69619834aa75e24
Dominik Drees <dominik.drees@uni-muenster.de>
2024-07-24 12:24:29 +0200

    Linearize jit node tree
    Tree is now a DAG, actually.

--- palace-core/src/jit.rs
+++ palace-core/src/jit.rs
@@ -13,7 +13,7 @@ use crate::{
     task::RequestStream,
     vulkan::{
         pipeline::{AsDescriptors, ComputePipeline, DescriptorConfig},
-        shader::ShaderDefines,
+        shader::{Config, ShaderDefines},
         state::RessourceId,
         DstBarrierInfo, SrcBarrierInfo,
     },
@@ -48,7 +48,7 @@ impl UnaryOp {
         })
     }
 }
-struct WriteUnary(UnaryOp, ValueId);
+struct WriteUnary(UnaryOp, NodeId);
 impl Display for WriteUnary {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.0 {
@@ -79,7 +79,7 @@ impl BinOp {
         }
     }
 }
-struct WriteBin(BinOp, ValueId, ValueId);
+struct WriteBin(BinOp, NodeId, NodeId);
 impl Display for WriteBin {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.0 {
@@ -114,32 +114,32 @@ impl ConstValue {
 #[derive(id::Identify, Clone)]
 enum Node {
     Const(ConstValue),
-    UnaryOp(DType, UnaryOp, Box<Node>),
-    BinOp(DType, BinOp, Box<Node>, Box<Node>),
+    UnaryOp(DType, UnaryOp, NodeId),
+    BinOp(DType, BinOp, NodeId, NodeId),
     Read(DType, InputId),
 }
 
 #[derive(Clone)]
-struct OperatorSet<D: DynDimension>(Vec<TensorOperator<D, DType>>);
+struct OrderedSet<T>(Vec<T>);
 
-impl<D: DynDimension> OperatorSet<D> {
-    fn add(&mut self, op: TensorOperator<D, DType>) -> usize {
+impl<T: Identify> OrderedSet<T> {
+    fn add(&mut self, item: T) -> usize {
         let pos = if let Some(pos) = self
             .0
             .iter()
             .enumerate()
-            .find_map(|(i, n)| (n.id() == op.id()).then_some(i))
+            .find_map(|(i, n)| (n.id() == item.id()).then_some(i))
         {
             pos
         } else {
             let pos = self.0.len();
-            self.0.push(op.clone());
+            self.0.push(item);
             pos
         };
         pos
     }
 
-    fn merge(mut self, other: OperatorSet<D>) -> Self {
+    fn merge(mut self, other: OrderedSet<T>) -> Self {
         for l in other.0.into_iter() {
             self.add(l);
         }
@@ -150,17 +150,18 @@ impl<D: DynDimension> OperatorSet<D> {
 //TODO: Rc for cheap clone?
 #[derive(Clone)]
 pub struct JitTensorOperator<D: DynDimension> {
-    node: Node,
+    root: NodeId,
     metadata: Option<TensorMetaData<D>>,
     dtype: DType,
-    operators: OperatorSet<D>,
+    operators: OrderedSet<TensorOperator<D, DType>>,
+    nodes: OrderedSet<Node>,
 }
 
 impl<D: DynDimension> id::Identify for JitTensorOperator<D> {
     fn id(&self) -> Id {
         // Note: All information (including operators via there ids) is present in the operation
         // tree `node`
-        self.node.id()
+        self.root.0
     }
 }
 
@@ -188,11 +189,16 @@ impl<D: DynDimension> JitTensorOperator<D> {
     pub fn unary_op(op: UnaryOp, inner: JitTensorOperator<D>) -> Result<Self, crate::Error> {
         Ok({
             let dtype = op.dtype(inner.dtype)?;
+            let node = Node::UnaryOp(dtype, op, inner.root);
+            let root = NodeId(node.id());
+            let mut nodes = inner.nodes;
+            nodes.add(node);
             Self {
+                root,
                 metadata: inner.metadata.clone(),
                 dtype,
-                node: Node::UnaryOp(dtype, op, Box::new(inner.node)),
                 operators: inner.operators,
+                nodes,
             }
         })
     }
@@ -203,6 +209,11 @@ impl<D: DynDimension> JitTensorOperator<D> {
     ) -> Result<Self, crate::Error> {
         Ok({
             let dtype = op.dtype(l.dtype, r.dtype)?;
+
+            let node = Node::BinOp(dtype, op, l.root, r.root);
+            let root = NodeId(node.id());
+            let mut nodes = l.nodes.merge(r.nodes);
+            nodes.add(node);
             Self {
                 metadata: match (l.metadata, r.metadata) {
                     (None, None) => None,
@@ -217,8 +228,9 @@ impl<D: DynDimension> JitTensorOperator<D> {
                     }
                 },
                 dtype,
-                node: Node::BinOp(dtype, op, Box::new(l.node), Box::new(r.node)),
                 operators: l.operators.merge(r.operators),
+                nodes,
+                root,
             }
         })
     }
@@ -226,11 +238,15 @@ impl<D: DynDimension> JitTensorOperator<D> {
 
 impl<D: DynDimension> From<ConstValue> for JitTensorOperator<D> {
     fn from(c: ConstValue) -> Self {
+        let node = Node::Const(c);
+        let root = NodeId(node.id());
+        let nodes = OrderedSet(vec![node]);
         Self {
-            node: Node::Const(c),
+            root,
+            nodes,
             metadata: None,
             dtype: c.dtype(),
-            operators: OperatorSet(Vec::new()),
+            operators: OrderedSet(Vec::new()),
         }
     }
 }
@@ -246,11 +262,16 @@ impl<D: DynDimension> From<TensorOperator<D, DType>> for JitTensorOperator<D> {
         let dtype = c.chunks.dtype();
         let id = c.chunks.id();
         let metadata = Some(c.metadata.clone());
+
+        let node = Node::Read(dtype, InputId(id));
+        let root = NodeId(node.id());
+        let nodes = OrderedSet(vec![node]);
         Self {
+            root,
+            nodes,
             metadata,
             dtype,
-            node: Node::Read(dtype, InputId(id)),
-            operators: OperatorSet(vec![c]),
+            operators: OrderedSet(vec![c]),
         }
     }
 }
@@ -285,61 +306,13 @@ impl Display for InputId {
     }
 }
 
-struct ValueId(usize);
+#[derive(id::Identify, Copy, Clone)]
+struct NodeId(Id);
 
-impl Display for ValueId {
+impl Display for NodeId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "v{}", self.0)
+        write!(f, "v{}", self.0.raw())
     }
-}
-
-#[derive(Default)]
-struct ValueIdGenerator {
-    next: usize,
-}
-impl ValueIdGenerator {
-    fn next(&mut self) -> ValueId {
-        self.next += 1;
-        ValueId(self.next)
-    }
-}
-
-fn translate(
-    n: &Node,
-    w: &mut dyn Write,
-    value_ids: &mut ValueIdGenerator,
-) -> Result<ValueId, std::fmt::Error> {
-    let res_id = value_ids.next();
-    match n {
-        Node::Const(c) => {
-            writeln!(w, "{} {} = {};", c.dtype().glsl_type(), res_id, c)?;
-        }
-        Node::UnaryOp(t, o, a) => {
-            let param = translate(a, w, value_ids)?;
-            writeln!(
-                w,
-                "{} {} = {};",
-                t.glsl_type(),
-                res_id,
-                WriteUnary(*o, param)
-            )?;
-        }
-        Node::BinOp(t, o, l, r) => {
-            let param_l = translate(l, w, value_ids)?;
-            let param_r = translate(r, w, value_ids)?;
-            writeln!(
-                w,
-                "{} {} = {};",
-                t.glsl_type(),
-                res_id,
-                WriteBin(*o, param_l, param_r)
-            )?;
-        }
-        Node::Read(t, v) => {
-            writeln!(w, "{} {} = {};", t.glsl_type(), res_id, v)?;
-        }
-    }
-    Ok(res_id)
 }
 
 pub fn jit<D: DynDimension>(op: TensorOperator<D, DType>) -> JitTensorOperator<D> {
@@ -347,12 +320,13 @@ pub fn jit<D: DynDimension>(op: TensorOperator<D, DType>) -> JitTensorOperator<D
 }
 
 fn compile<D: DynDimension>(
-    node: &Node,
+    nodes: &Vec<Node>,
+    root: NodeId,
     inputs: &Vec<TensorOperator<D, DType>>,
-) -> Result<String, crate::Error> {
+) -> Result<(String, Config), crate::Error> {
     let mut shader = String::new();
+    let mut config = Config::new();
 
-    writeln!(&mut shader, "#version 450")?;
     writeln!(&mut shader, "layout (local_size_x = 256) in;")?;
 
     for (i, input) in inputs.iter().enumerate() {
@@ -400,8 +374,43 @@ fn compile<D: DynDimension>(
         )?;
     }
 
-    let mut value_ids = ValueIdGenerator::default();
-    let res = translate(&node, &mut shader, &mut value_ids)?;
+    for node in nodes {
+        let res_id = NodeId(node.id());
+        let dtype = match node {
+            Node::Const(c) => {
+                writeln!(&mut shader, "{} {} = {};", c.dtype().glsl_type(), res_id, c)?;
+                c.dtype()
+            }
+            Node::UnaryOp(t, o, a) => {
+                //let param = translate(a, w, value_ids)?;
+                writeln!(
+                    &mut shader,
+                    "{} {} = {};",
+                    t.glsl_type(),
+                    res_id,
+                    WriteUnary(*o, *a)
+                )?;
+                *t
+            }
+            Node::BinOp(t, o, l, r) => {
+                //let param_l = translate(l, w, value_ids)?;
+                //let param_r = translate(r, w, value_ids)?;
+                writeln!(
+                    &mut shader,
+                    "{} {} = {};",
+                    t.glsl_type(),
+                    res_id,
+                    WriteBin(*o, *l, *r)
+                )?;
+                *t
+            }
+            Node::Read(t, v) => {
+                writeln!(&mut shader, "{} {} = {};", t.glsl_type(), res_id, v)?;
+                *t
+            }
+        };
+        config = config.ext(dtype.glsl_ext());
+    }
 
     writeln!(
         &mut shader,
@@ -410,10 +419,10 @@ fn compile<D: DynDimension>(
                 }}
             }}
             "#,
-        res,
+        root,
     )?;
 
-    Ok(shader)
+    Ok((shader, config))
 }
 impl<D: DynDimension> JitTensorOperator<D> {
     pub fn compile(self) -> Result<TensorOperator<D, DType>, crate::Error> {
@@ -444,12 +453,18 @@ impl<D: DynDimension> JitTensorOperator<D> {
                             .of(ctx.current_op())
                             .dependent_on(jit_operator),
                         || {
-                            let shader = compile(&jit_operator.node, &jit_operator.operators.0)?;
+                            let (shader, config) = compile(
+                                &jit_operator.nodes.0,
+                                jit_operator.root,
+                                &jit_operator.operators.0,
+                            )?;
+                            println!("{}", shader.as_str());
                             ComputePipeline::new(
                                 device,
                                 (
-                                    dbg!(shader.as_str()),
+                                    shader.as_str(),
                                     ShaderDefines::new().add("BRICK_MEM_SIZE", num_chunk_elements),
+                                    config,
                                 ),
                                 true,
                             )
