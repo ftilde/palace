a2397a2c76211a0abe167560d278e556659cac48
Dominik Drees <dominik.drees@uni-muenster.de>
2024-10-22 16:38:23 +0200

    Fix procedural tensor generation ressource id

--- palace-core/src/operators/procedural.rs
+++ palace-core/src/operators/procedural.rs
@@ -1,4 +1,5 @@
 use ash::vk;
+use id::Identify;
 
 use crate::{
     array::{ImageMetaData, TensorEmbeddingData, TensorMetaData, VolumeMetaData},
@@ -198,6 +199,8 @@ void main()
 "#
     );
 
+    let shader_id = shader.id();
+
     TensorOperator::with_state(
         OperatorDescriptor::new("rasterize_gpu")
             .dependent_on_data(gen_fn)
@@ -214,6 +217,7 @@ void main()
                 let pipeline = device.request_state(
                     RessourceId::new("pipeline")
                         .of(ctx.current_op())
+                        .dependent_on(&shader_id)
                         .dependent_on(&m.chunk_size)
                         .dependent_on(&D::N),
                     || {
