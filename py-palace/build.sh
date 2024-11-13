36d249199a3db2e65f7c94f4beed17de0f1fae78
Dominik Drees <dominik.drees@uni-muenster.de>
2024-11-13 16:26:53 +0100

    Fix pypalace build script to avoid constant rebuilds

--- py-palace/build.sh
+++ py-palace/build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-#export PYO3_PYTHON=$(realpath ./venv/bin/python)
-#export PYO3_ENVIRONMENT_SIGNATURE=cpython-3.12-64bit
+
+set -e
+
+# Wow, trying to avoid rebuilds with pyo3/maturin really sucks. See
+# https://github.com/PyO3/PyO3/issues/1708
+
+export script_dir=$(dirname $(realpath $0))
+export PYO3_PYTHON=$script_dir/venv/bin/python
+export PYO3_ENVIRONMENT_SIGNATURE=cpython-3.12-64bit
+export CARGO_TARGET_DIR=maturin_target
+#export CARGO_LOG=cargo::core::compiler::fingerprint=info
 
 cargo run --bin=stub_gen "$@"
-CARGO_TARGET_DIR=maturin_target maturin develop "$@"
+maturin develop "$@"
