5cf208f3ff09a629c12cab5b0f3aef6ec62c8e07
Dominik Drees <dominik.drees@uni-muenster.de>
2025-01-14 17:31:04 +0100

    Add tentative support for 4d tensors in randomwalker.py

--- py-palace/randomwalker.py
+++ py-palace/randomwalker.py
@@ -26,20 +26,38 @@ args = parser.parse_args()
 
 rt = pc.RunTime(ram_size, vram_size, disk_cache_size, device=0)
 
-vol = pc.open_or_create_lod(args.volume_file, rechunk=True, chunk_size_hint=[32,32,32])
+try:
+    vol = pc.open_lod(args.volume_file)
+except:
+    vol = pc.open(args.volume_file)
+    #vol = vol.rechunk([32]*vol.nd())
+    #vol = vol[:5,:,:,:]
+    steps = list(reversed([2.0 if i < 3 else pc.FixedStep(2.0) for i in range(0, vol.nd())]))
+    vol = vol.create_lod(steps)
+    #vol = vol.single_level_lod()
 
-ndim = len(vol.levels[0].inner.metadata.dimensions)
+nd = vol.nd()
 
-foreground_seeds = np.empty(shape=[0,ndim], dtype=np.float32)
-background_seeds = np.empty(shape=[0,ndim], dtype=np.float32)
+foreground_seeds = np.empty(shape=[0,nd], dtype=np.float32)
+background_seeds = np.empty(shape=[0,nd], dtype=np.float32)
 
 if args.foreground_seeds:
     foreground_seeds = np.concat([foreground_seeds, read_vge(args.foreground_seeds)])
 if args.background_seeds:
     background_seeds = np.concat([background_seeds, read_vge(args.background_seeds)])
 
-md: pc.TensorMetaData = vol.levels[0].inner.metadata
-ed = vol.levels[0].embedding_data
+def select_vol_from_ts(v, ts):
+    match nd:
+        case 3:
+            return v
+        case 4:
+            return palace_util.slice_time_4d(ts, v)
+        case o:
+            raise f"Invalid number of tensor dimensions: {o}"
+
+v0 = select_vol_from_ts(vol, 0)
+l0md: pc.TensorMetaData = v0.fine_metadata()
+l0ed = v0.fine_embedding_data()
 
 vol = vol.map(lambda vol: vol.cast(pc.ScalarType.F32))
 vol = vol.map(lambda vol: vol * (1.0/(1 << 16)))
@@ -48,6 +66,9 @@ if args.transfunc:
     tf = pc.load_tf(args.transfunc)
 else:
     tf = pc.grey_ramp_tf(0.0, 1.0)
+
+def fit_tf_to_values(vol):
+    palace_util.fit_tf_range(rt, vol.levels[0], tf)
 
 num_tf_values = 128
 def prob_tf_from_values(values):
@@ -60,16 +81,18 @@ tf_prob3d = prob_tf_from_values([[0, 0, 0, 0] for i in range(num_tf_values)] + [
 
 store = pc.Store()
 
-slice_state0 = pc.SliceviewState.for_volume(md, ed, 0).store(store)
-slice_state1 = pc.SliceviewState.for_volume(md, ed, 1).store(store)
-slice_state2 = pc.SliceviewState.for_volume(md, ed, 2).store(store)
-camera_state = pc.CameraState.for_volume(md, ed, 30.0).store(store)
+slice_state0 = pc.SliceviewState.for_volume(l0md, l0ed, 0).store(store)
+slice_state1 = pc.SliceviewState.for_volume(l0md, l0ed, 1).store(store)
+slice_state2 = pc.SliceviewState.for_volume(l0md, l0ed, 2).store(store)
+camera_state = pc.CameraState.for_volume(l0md, l0ed, 30.0).store(store)
 raycaster_config = pc.RaycasterConfig().store(store)
 raycaster_config_rw = pc.RaycasterConfig().store(store)
+timestep = store.store_primitive(0)
 
 #raycaster_config_rw.compositing_mode().write("DVR")
 
 mode = store.store_primitive("hierarchical")
+#mode = store.store_primitive("normal")
 weight_function = store.store_primitive("bian_mean")
 min_edge_weight = store.store_primitive(1e-5)
 
@@ -88,18 +111,18 @@ def apply_weight_function(volume):
             return pc.randomwalker_weights_bian(volume, min_edge_weight.load(), extent.load())
 
 def apply_rw_mode(input):
-
     fg_seeds_tensor = pc.from_numpy(foreground_seeds).fold_into_dtype()
     bg_seeds_tensor = pc.from_numpy(background_seeds).fold_into_dtype()
 
     match mode.load():
         case "normal":
-            i = input.levels[0].rechunk([pc.chunk_size_full]*3)
+            i = input.levels[0].rechunk([pc.chunk_size_full]*nd)
             md: pc.TensorMetaData = i.inner.metadata
+            ed = i.embedding_data
             weights = apply_weight_function(i)
             seeds = pc.rasterize_seed_points(fg_seeds_tensor, bg_seeds_tensor, md, ed)
             rw_result = pc.randomwalker(weights, seeds, max_iter=1000, max_residuum_norm=0.001)
-            return (input, rw_result.create_lod(2.0))
+            return (input, rw_result.single_level_lod())
 
         case "hierarchical":
             weights = input.map(lambda level: apply_weight_function(level.inner).embedded(pc.TensorEmbeddingData(np.append(level.embedding_data.spacing, [1.0])))).cache_coarse_levels()
@@ -111,6 +134,9 @@ def render(size, events: pc.Events):
     global mouse_pos_and_value
 
     v, rw_result = apply_rw_mode(vol)
+
+    v = select_vol_from_ts(v, timestep.load())
+    rw_result = select_vol_from_ts(rw_result, timestep.load())
 
     # GUI stuff
     widgets = []
@@ -124,22 +150,38 @@ def render(size, events: pc.Events):
             widgets.append(palace_util.named_slider("extent", extent, 1, 5))
 
     widgets.append(palace_util.named_slider("min_edge_weight", min_edge_weight, 1e-20, 1, logarithmic=True))
+    widgets.append(pc.Button("Fit Transfer Function", lambda: fit_tf_to_values(v)))
 
     if mouse_pos_and_value is not None:
         vol_pos, value = mouse_pos_and_value
         widgets.append(pc.Label(f"Value at {vol_pos} = {value}"))
         mouse_pos_and_value = None
 
+    if nd != 3:
+        widgets.append(palace_util.named_slider("Timestep", timestep, 0, vol.fine_metadata().dimensions[0]-1))
+
     def add_seed_point(slice_state, embedded_tensor, pos, frame_size, foreground):
         global foreground_seeds, background_seeds
 
         pos3d = palace_util.mouse_to_volume_pos(slice_state.load(), embedded_tensor, pos, frame_size)
         if pos3d is not None:
-            pos3d = np.array(pos3d, dtype=np.float32).reshape((1, 3))
+            match nd:
+                case 3:
+                    pos_nd = np.array(pos3d, dtype=np.float32)
+                case 4:
+                    pos_nd = [timestep.load()] + list(pos3d)
+                    pos_nd = np.array(pos_nd, dtype=np.float32)
+
+            pos_nd = np.concatenate(([1], pos_nd), dtype = np.float32)
+            pos_nd = vol.fine_embedding_data().voxel_to_physical().dot(pos_nd)
+            pos_nd = pos_nd[1:]
+
+            pos_nd = pos_nd.reshape((1, nd))
+
             if foreground:
-                foreground_seeds = np.concat([foreground_seeds, pos3d])
+                foreground_seeds = np.concat([foreground_seeds, pos_nd])
             else:
-                background_seeds = np.concat([background_seeds, pos3d])
+                background_seeds = np.concat([background_seeds, pos_nd])
 
     def overlay_slice(state):
 
