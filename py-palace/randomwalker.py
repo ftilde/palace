b327f9385d38b484e88d90667379d9d1d4753b96
Dominik Drees <dominik.drees@uni-muenster.de>
2025-02-14 16:14:13 +0100

    Default to var_gaussian in randomwalker.py

--- py-palace/randomwalker.py
+++ py-palace/randomwalker.py
@@ -128,7 +128,7 @@ raycaster_config_rw.compositing_mode().write("DVR")
 
 mode = store.store_primitive("hierarchical")
 #mode = store.store_primitive("normal")
-weight_function = store.store_primitive("bian_mean")
+weight_function = store.store_primitive("var_gaussian")
 min_edge_weight = store.store_primitive(1e-5)
 
 beta = store.store_primitive(128.0)
