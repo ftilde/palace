0bcbb3e8460841bfbee0bca3a1b8e46618c2c532
Dominik Drees <dominik.drees@uni-muenster.de>
2025-09-09 12:21:01 +0200

    Default to bian-mean in randomwalker.py

--- py-palace/randomwalker.py
+++ py-palace/randomwalker.py
@@ -141,7 +141,9 @@ raycaster_config_rw.compositing_mode().write("DVR")
 
 mode = store.store_primitive("hierarchical")
 #mode = store.store_primitive("normal")
-weight_function = store.store_primitive("bhatt_var_gaussian")
+#weight_function = store.store_primitive("bhatt_var_gaussian")
+#weight_function = store.store_primitive("ttest")
+weight_function = store.store_primitive("bian_mean")
 min_edge_weight = store.store_primitive(1e-5)
 
 beta = store.store_primitive(128.0)
